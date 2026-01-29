0231cfd5ba37ef7fab565b19bc1d9656162b07a6
nadiasapano <nadiasapano@gmail.com>
2026-01-29 11:11:21 -0500

    Update app.java

--- Assignment 1/app.java
+++ Assignment 1/app.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class App {
  public static void main(String[] args) {
  //finish this one for me by receiving and printing the user's age back to them
+ Scanner sc = new Scanner(System.in);
  System.out.println("How old are you? ");
+ String age = sc.nextLine();
+ System.out.println("I am " + age + " years old.");
  //can you do some math for me? take these variables and print out the sum of them!
  int num1 = 10;
  int num2 = 37;
- //?
+ System.out.println(num1 + num2);
+ // I learned how to take user input in Java using a scanner. I also learned that Java is ten time more annoying then Python, and that I miss python. 
+        // I also relearned how to reassign variables and was refreshed on all the different data types i.e. strings, integers, booleans, etc
+        // Looking forward to a great semester! 
  }
 }
