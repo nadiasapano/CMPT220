682b865296c549d0316ca039d91d2464d1e1e946
nadiasapano <nadiasapano@gmail.com>
2026-04-10 12:54:00 -0400

    Create Enemy.java

---
+++ Assignment 7/Enemy.java
@@ -0,0 +1,14 @@
+public class Enemy {
+    int health;
+    int damage;
+
+    public Enemy(int h, int d){
+        health = h;
+        damage = d;
+    }
+
+    void attack(){
+        System.out.println("the enemy attacks");
+    }
+}
+
